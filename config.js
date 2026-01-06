c9459342f1a74fca0831e1fb8c1f5c50e196982b
Maynard G. Krebs <chrisharges@gmail.com>
2026-01-05 19:19:47 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const APP_CONFIG = {
+  appName: "Anthodyne",
+  backendBase: "https://anthodyne-backend.onrender.com",
+  defaultTopic: "All",
+  // add more instance inputs here as you find them
+};
+
+const API_URL = `${APP_CONFIG.backendBase}/api/articles`;
+const SONNET_URL = `${APP_CONFIG.backendBase}/api/sonnet`;
